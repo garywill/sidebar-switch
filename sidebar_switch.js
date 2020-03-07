e407c47256b4f164f4575a8531f386460ca08d56
Gary W <32130780+garywill@users.noreply.github.com>
2020-03-07 06:58:13 +0000

    Update sidebar_switch.js

--- sidebar_switch.js
+++ sidebar_switch.js
@@ -1,4 +1,9 @@
-// tested on firefox 68
+/* Firefox userChrome script
+ * Tested on Firefox 68
+ * Author: garywill (https://github.com/garywill)
+ * https://github.com/garywill/sidebar-switch
+ */
+
 console.log("sidebar_switch.js");
 
 (() => {
@@ -6,18 +11,41 @@ console.log("sidebar_switch.js");
     
     if ( bbrowser && document.getElementById("main-window") && document.getElementById("TabsToolbar") && document.getElementById("urlbar-container") )
     {
+        var switcher_c =  document.createElement("vbox");
         var switcher =  document.createElement("vbox");
         
         switcher.id = "sidebar_switcher";
+        switcher_c.id = "sidebar_switcher_c";
+ 
+        switcher_c.style.position = "relative";
+        
         switcher.style.width="6px";
         switcher.style.minWidth="6px";
         switcher.style.maxWidth="6px";
+        switcher.style.position = "absolute";
+        switcher.style.backgroundColor = "blue"; 
         
-        switcher.addEventListener('click', function(){
-            SidebarUI.toggle();
+        // in css file
+        //switcher.style.opacity = "0.2";
+        
+        switcher.tooltipText = "Click to toggle sidebar"
+ 
+        switcher_c.appendChild(switcher);
+        bbrowser.insertBefore(switcher_c, bbrowser.childNodes[0]);
+        
+        switcher.addEventListener('click', async function(){
+            await SidebarUI.toggle();
+            /*
+            if (SidebarUI.isOpen) 
+            {
+                switcher_c.style.position = "";
+            } 
+            else 
+            {
+                switcher_c.style.position = "relative";
+            }
+            */
         });
-        
-        bbrowser.insertBefore(switcher, bbrowser.childNodes[0]);
         
         
         const nav_tb = document.getElementById("navigator-toolbox");
@@ -26,17 +54,20 @@ console.log("sidebar_switch.js");
             {
                 if(document.fullscreen) // video fullscreen
                 {   
-                    switcher.style.display = "none"; // hide
+                    switcher_c.style.display = "none"; // hide
                 }
             }
             else // not fullscreen
             {
-                switcher.style.display = "";  
+                switcher_c.style.display = "";  
             }
             
         });
         tabsbar_fullscr_observer.observe(nav_tb,{attributes:true});
         
-
+        window.addEventListener('resize', function(){
+            switcher.style.height = bbrowser.boxObject.height + "px";
+        });
     }
 })();
+
